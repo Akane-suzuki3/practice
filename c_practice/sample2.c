99514aefffd2fbbde74cb911d1e1c1452674e0c2
Akane-suzuki3 <akp.hq282@gmail.com>
2025-09-16 17:37:39 +0900

    add sample codes for C and Python

---
+++ c_practice/sample2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    int x;
+    printf("数字を入力してください: ");
+    scanf("%d", &x);
+    printf("入力した数字は %d です\n", x);
+    return 0;
+}
---
+++ c_practice/sample3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    for(int i=0; i<5; i++){
+        printf("%d 回目のHello\n", i+1);
+    }
+    return 0;
+}
